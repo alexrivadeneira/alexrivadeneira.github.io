52ced34cab5336a178e9e30bb6274b8edb3a495f
alexrivadeneira <alexrivs@gmail.com>
2018-06-02 09:27:09 -0700

    edit update

--- content/home.js
+++ content/home.js
@@ -2,32 +2,32 @@ const homePosts = [
 {
     "title":"Experimenting with CSS Animations",
     "date":"05/30/2018",
-    "content":`<p>I've been experimenting with CSS animations, and used them added a little visual interest to my blog.  For those of you who want to learn more about getting started with CSS animations, here's some information to get started.  We can create some really impressive effects using only CSS (no flash or JavaScript!), just modern web browsers.</p>
+    "content":`<p>CSS animations let us create some really impressive effects using only CSS (no Flash or JavaScript!) I've been experimenting with them to add a little life to my blog. For those of you who want to learn more about getting started with CSS animations, here's a simple guide.  </p>
 
     <p>The easiest way to explain how this works is with an example.</p>
     
-    <p>Let's say we have a div, we'll give it some basic style properties:</p>
+    <p>Let's say we have a div; we'll give it some basic style properties:</p>
     
     <pre><div id="redbox"></div></pre>
     
     #redbox{ background: red; width: 100px; height: 100px;
     } 
     
-    <p>Now, if we want to do an animation, all we have to do is create an name an animation in the CSS file like so.  We use a new declaration called keyframes to label our animation:</p>
+    <p>Now, if we want to animate our little red box, all we have to do is declare an animation in the CSS file like so.  We use a new declaration called keyframes to label our animation:</p>
     
     <pre>@keyframes myAnimation{
     }
     </pre>
     
-    <p>Inside the @keyframes declaration, we specify different CSS changes we want at different times.  </p>
+    <p>Inside the @keyframes declaration, we specify how we want our CSS to look at different moments in time. </p>
 
-    <p>In a lot of ways, this is like a more complicated and powerful version of the CSS psuedo selectors.  To remind you about that, here's an example of how we would change our redbox using a CSS psuedoselector.  If we wanted to change the size and color of the redbox on hover, here's how we would do it using a pseduoselector:</p>
+    <p>In a lot of ways, this is like a more complicated and powerful version of the CSS psuedo selectors.  To remind you about those, here's an example of how we would change our red box using a CSS psuedoselector for the hover state:</p>
     
     <pre>#redbox:hover{ width: 200px; height: 200px; background: green;
     }
     </pre>
     
-    <p>This is pretty effective, but the transition is a little jarring.</p>
+    <p>In this case, when the user hovers over the red box, it changes sizes and colors. This is pretty effective, but the transition is a little jarring.</p>
     
     <p>Without even returning to keyframes, we could "ease" the pseduo selector transition a bit using a new CSS property on the #redbox div:</p>
     
@@ -39,9 +39,9 @@ const homePosts = [
     </pre>
     
     <p>
-    This new transition property will "ease" in any change over a period of time that we specify, in this case 1 second.  When the hover effect gets targeted, the change will happen and it will be eased. </p>
+    This new transition property will "ease" in any change over a period of time that we specify, in this case 1 second.  When the hover effect gets targeted, the change will happen and it will be eased, smoothing the transition between the two states. </p>
     
-    <p>Okay, but now back to the keyframes feature.  Let's say we want an even more complicated animation that doesn't involve just two states.  Let's say we want to animate our div so that it phases through multiple colors and sizes before returning to its original state.  The keyframes are basically animation declarations at different times along a timeline of the animation.  We can start at 0% and declare different styles all the way through 100%.</p>
+    <p>Okay, but now back to the keyframes feature.  Let's say we want to design a more complicated animation that doesn't involve just two states.  Let's say we want to animate our div so that it phases through multiple colors and sizes before returning to its original state.  Keyframes let us build different CSS styles at different moments in time.  We can start at 0% and declare different styles all the way through 100%.</p>
     
     <p>For example, if we want our box to transition from red to blue to green and back to red, we can write something like this:</p>
     
@@ -54,11 +54,11 @@ const homePosts = [
     </pre>
     
     <p>
-    Now all we have to do is add this keyframe property to our redbox div.  In this case, we'll also remove our redbox psuedoselector.
+    Now all we have to do is add this keyframe property to our redbox div.  (In this case, we'll also remove our redbox psuedoselector.)
     </p>
 
     <pre>
-    #redbox{ animation: myAnimation }
+    #redbox{ animation: myAnimation; }
     </pre>
 
     <p>
@@ -70,7 +70,7 @@ const homePosts = [
     </pre>
 
     <p>
-    In this case, we're specifying that redbox will run myAnimation (the keyframes declaration above) over the course of 15 seconds and will repeat this infitinitely!
+    In this case, I've declared that redbox will run myAnimation (the keyframes declaration above) over the course of 15 seconds and will repeat this infitinitely!
     </p>
 
     <p>
