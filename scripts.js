717f1f0cd42eb5429543e68f1a3440cde6dec288
Alex Rivadeneira <alexrivadeneira@asana.com>
2020-08-02 14:51:08 -0700

    add map

--- scripts.js
+++ scripts.js
@@ -1,25 +1,10 @@
-// // Parallax effect
-// // Adapted from @ilonacodes article ->  https://link.medium.com/7fFiON6Q1X
-//
-// // Update : added throttle to increase performance
-// window.addEventListener('scroll', throttle(parallax, 14));
-//
-// function throttle(fn, wait) {
-//   var time = Date.now();
-//   return function() {
-//     if ((time + wait - Date.now()) < 0) {
-//       fn();
-//       time = Date.now();
-//     }
-//   }
-// };
-//
-// function parallax() {
-//   var scrolled = window.pageYOffset;
-//   var parallax = document.querySelector(".parallax");
-//   // You can adjust the 0.4 to change the speed
-// 	var coords = (scrolled * 0.4) + 'px'
-//   parallax.style.transform = 'translateY(' + coords + ')';
-// };
-//
-// console.log('scripts');
+
+const mapStart = document.querySelector('#map img');
+mapStart.style.transform = 'translate3d(-1477.71px, -2334.35px, 0px)';
+
+Draggable.create("#map img", {
+   type:"x,y",
+	   bounds:"#wrapper",
+	   edgeResistance:0.5,
+     intertia: true,
+});
