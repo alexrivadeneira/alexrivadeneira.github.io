c099aa8e1a6e37e911767df50bcd572c441ec263
Alex Rivadeneira <alexrivadeneira@asana.com>
2020-09-10 18:59:26 -0700

    remove map add sierra club image

--- scripts.js
+++ scripts.js
@@ -1,13 +1,5 @@
 
-const mapStart = document.querySelector('#map img');
-mapStart.style.transform = 'translate3d(-1718.7px, -2302.15px, 0px)';
 
-Draggable.create("#map img", {
-   type:"x,y",
-	   bounds:"#wrapper",
-	   edgeResistance:0.5,
-     intertia: true,
-});
 
 var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 var inputActivities = document.querySelector('input[name="activity"]');
